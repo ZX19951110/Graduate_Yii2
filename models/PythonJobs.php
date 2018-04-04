1c0492c5a9b5e9198bec02c102a06637300b68a1
ZX19951110 <619186893@qq.com>
2018-04-04 18:23:52 +0200

    2018年04月 4日 周三 下午  6:23:50 后端可和数据库交互并返回json

---
+++ models/PythonJobs.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/3 0003
+ * Time: 下午 2:22
+ */
+namespace app\models;
+use yii\db\ActiveRecord;
+class PythonJobs extends ActiveRecord{
+
+    public static function tableName()
+    {
+        return "python";
+    }
+}
